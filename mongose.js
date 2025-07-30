723e283b02f700cd077a9cd3ad9c9d023a4eb7ea
Raaz <rajkumar109910@gmail.com>
2025-07-30 13:16:22 +0530

    moongoose crud

--- mongose.js
+++ mongose.js
@@ -1,21 +1,37 @@
 const mongoose = require('mongoose');
-const url = 'mongodb://localhost:27017/code-step-by-step';
-mongoose.connect(url)
-    .then(() => console.log('MongoDB connected successfully'))
-    .catch(err => console.log('MongoDB connection error:', err));
+const url = 'mongodb://localhost:27017/'; // MongoDB connection URL
+const dbName = 'code-step-by-step'; // Database name
+mongoose.connect(url + dbName);
+const schema = new mongoose.Schema({
+    name: String,
+    age: Number
+})
+const collection = mongoose.model('node-tut', schema, 'node-tut');
+// Collection name
+const saveData = async () => {
+    const data = new collection({
+        name: 'John Doe...',
+        age: 30
+    });
+    await data.save()
+        .then(() => console.log('Data saved successfully'))
+        .catch(err => console.error('Error saving data:', err))
 
-const schema = mongoose.Schema;
+}
+//saveData();
 
-const userschema = new schema({ name: String });
+//update data
+const updateData = async () => {
+    const data = await collection.updateOne({ name: "John Doe" }, { $set: { age: 31 } })
+    console.log(data)
+}
+//updateData().then(() => console.log('Data updated successfully'))
+//.catch(err => console.error('Error updating data:', err));
 
-const User = mongoose.model('node-tut', userschema, 'node-tut');
-
-const newUser = new User({
-    name: 'Static Raj'
-});
-
-newUser.save()
-    .then(() => console.log('Static data saved'))
-    .catch(err => console.log(err));
-
+//delete data
+const deleteData = async () => {
+    const data = await collection.deleteOne({ name: "John Doe" })
+    console.log(data)
+}
+deleteData();
 
